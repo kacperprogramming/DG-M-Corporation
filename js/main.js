d19f2654086d85627c6073bb2b40d9023276f8cc
Kacper <loliks.elix@gmail.com>
2026-05-04 00:19:16 +0200

    add footer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const burgerIcon = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav__links-mobile')
 const navMobileLinks = document.querySelectorAll('.nav__link-mobile')
+const footerText = document.querySelector('.footer__bottom-text')
 
 const handleMobileNav = () => {
     burgerIcon.classList.toggle('is-active')
     navMobile.classList.toggle('nav-active')
+}
+
+const handleFooterYear = () => {
+    const now = new Date().getFullYear()
+
+    footerText.innerHTML = `&copy; ${now} Tropical Paradise`
 }
 
 burgerIcon.addEventListener('click', handleMobileNav)
@@ -14,3 +21,5 @@ navMobileLinks.forEach(link => {
         navMobile.classList.remove('nav-active')
     })
 })
+
+handleFooterYear()
